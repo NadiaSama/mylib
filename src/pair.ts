2352c11ce4c14515d023b04ee9406af99483e732
= <=wangyue@digifinex.com>
2020-11-05 19:08:57 +0800

    增加代码 developing

---
+++ src/pair.ts
@@ -0,0 +1,7 @@
+import { Token } from './token';
+export class Pair {
+  token0: Token;
+  token1: Token;
+
+  constructor(addrA: string, addrB: string) {}
+}
---
+++ src/token.ts
@@ -0,0 +1,8 @@
+export class Token {
+  symbol: string;
+  amount: string;
+  constructor(symbol: string, amount: string) {
+    this.symbol = symbol;
+    this.amount = amount;
+  }
+}
